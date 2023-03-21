c2639d7a5595b619a1f07cdbb061db8cc7ec25e9
Duane Moody <duane_moody@yahoo.com>
2023-03-21 09:58:27 -0700

    Basic functionality
    JS works, need to test whether it works as a userscript

--- whotwibutton.js
+++ whotwibutton.js
@@ -6,3 +6,24 @@
 // @match				https://mobile.twitter.com/*
 // @grant    		none
 // ==/UserScript==
+
+// Get userid
+// this method will only work for profile pages, not popup profiles 
+const username = location.pathname.split('/')[1];
+
+// Get destination
+const whotwi = `https://en.whotwi.com/${username}/tweets/media#local_local_menu`;
+
+// Create link
+const whobut = document.createElement('a'),
+      tn = document.createTextNode('WT');
+whobut.appendChild(tn);
+whobut.setAttribute('href', whotwi);
+whobut.setAttribute('target', '_blank');
+
+// Get location to place link
+const buttonboxclass = "css-1dbjc4n r-obd0qt r-18u37iz r-1w6e6rj r-1h0z5md r-dnmrzs", 
+      buttonbox = document.getElementsByClassName(buttonboxclass)[0];
+
+// place link
+buttonbox.prepend(whobut);
