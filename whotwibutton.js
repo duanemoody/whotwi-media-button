62cf10bea11a25aaf6047261d904afcb1d775d32
Duane Moody <duane_moody@yahoo.com>
2023-03-21 09:02:04 -0700

    Create whotwibutton.js
    Initial commit

---
+++ whotwibutton.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name 		    Whotwi media link for Twitter
+// @version 		0.5
+// @description	Adds whotwi media link to a Twitter user profile
+// @match				https://twitter.com/*
+// @match				https://mobile.twitter.com/*
+// @grant    		none
+// ==/UserScript==
